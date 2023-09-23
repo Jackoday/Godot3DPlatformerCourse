77762c54b168407b460c2a1c39f27b79dba8a2a0
Jackoday <jacksonday@hotmail.co.nz>
2023-09-23 17:16:31 +1200

    Lesson 9 complete

---
+++ hazards/spike_roller.gd
@@ -0,0 +1,7 @@
+extends AnimatableBody3D
+
+
+
+func _on_hit_area_body_entered(body):
+	if body.has_method("respawn"):
+		body.respawn()
---
+++ items/diamond.gd
@@ -0,0 +1,6 @@
+extends Area3D
+
+
+
+func _on_body_entered(_body):
+	$AnimationPlayer.play("pick_up")
